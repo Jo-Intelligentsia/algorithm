4bf7a97326769690f8271c3ab820dffe7c47bceb
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-01-16 11:40:30 +0900

    [Bronze V] Title: A+B - 7, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,5 +1,5 @@
 T = int(input())
 
 for i in range(1,T+1):
-    a,b = map(int,input().split())
-    print(f'Case #{i}: {a+b}')
+    n1,n2 = map(int,input().split())
+    print(f'Case #{i}: {n1+n2}')
