78935a00a2ad7f0cd4d423b69f214fce29eda6d7
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-03-02 21:52:37 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,7 +1,11 @@
+# def solution(num1, num2):
+#     answer = 0
+#     if num1 == num2:
+#         answer = 1
+#     else:
+#         answer = -1
+#     return answer
+
 def solution(num1, num2):
-    answer = 0
-    if num1 == num2:
-        answer = 1
-    else:
-        answer = -1
-    return answer
+    return 1 if num1 == num2 else -1 
+
