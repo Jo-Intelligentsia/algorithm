486769c9995f687c7721305ae8d08323b35b3dbd
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-03-05 23:08:26 +0900

    [level 0] Title: 두 수의 차, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.py
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.py
@@ -1,3 +1,5 @@
-def solution(num1, num2):
-    answer = num1-num2
-    return answer
+# def solution(num1, num2):
+#     answer = num1-num2
+#     return answer
+
+solution = lambda num1, num2 : num1-num2
