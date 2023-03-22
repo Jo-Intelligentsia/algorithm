510db98a77af11b8099b260db9475babb8366666
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-03-22 18:41:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    answer = {'zero' : 0, 'one':1, 'two':2, 'three' : 3, 'four':4, 
+                 'five':5,'six' : 6, 'seven' : 7, 'eight':8,'nine':9}
+    for k, v in answer.items():
+        numbers = numbers.replace(k, str(v))
+    return int(numbers)
