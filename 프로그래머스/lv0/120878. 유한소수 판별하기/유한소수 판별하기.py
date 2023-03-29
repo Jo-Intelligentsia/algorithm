7e3d05936b0e6003827dc3b5e8b092f0a80ff00d
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-03-29 16:56:39 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,8 +1,11 @@
 from math import gcd
+# gcd 최대공약수 함수 사용
 def solution(a, b):
-    b //= gcd(a,b)
-    while b%2==0:
-        b//=2
-    while b%5==0:
-        b//=5
-    return 1 if b==1 else 2
+    # b //= gcd(a,b) # a,b 의 최대공약수
+    d = b//gcd(a,b)
+    print(d)
+    while d%2==0:
+        d//=2
+    while d%5==0:
+        d//=5
+    return 1 if d==1 else 2
