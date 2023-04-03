0da2b8567f14c666a29ff1404b459be310d4f299
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-04-03 20:39:04 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -1,9 +1,12 @@
+# def solution(array):
+#     answer = 0
+#     for i in array:
+#         for j in str(i):
+#             if '7' in str(j):
+#                 answer += 1
+#     return answer
+
+
 def solution(array):
     answer = 0
-    for i in array:
-        for j in str(i):
-            print(j)
-            if '7' in str(j):
-                answer += 1
-        print(i)
-    return answer
+    return str(array).count('7')
