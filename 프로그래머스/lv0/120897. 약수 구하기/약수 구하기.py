233f1092b303db9458a8f8a939429d329bc1b6f4
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-04-09 18:07:42 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = []
+    a = 0
+    # 1 ~ n 까지 반복
+    while True:
+        if n < a:
+            break
+        else:
+            a +=1
+            if n%a ==0:
+                answer.append(n//a)
+            
+    return sorted(answer)
