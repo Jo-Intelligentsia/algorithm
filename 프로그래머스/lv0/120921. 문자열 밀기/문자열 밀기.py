06dd3e7cdd746e45fdb5e30a824d1d1df21dae3f
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-04-12 08:18:28 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,14 +1,17 @@
 def solution(A, B):
     answer = 0
+    # 같으면 0 
     if A == B :
         answer = 0
+    # 다른 문자가 있으면 -1
     elif sorted(A) != sorted(B):
         answer = -1
-        print(sorted(A))
     else:
         while True:
+            # A,B 같아지면 break
             if A == B:
                 break
+            # 총 횟수가 A 의 문자열 개수와 같아지면 옮겼을때 같을수가 없으므로
             elif answer == len(A):
                 answer = -1
                 break
