11643508350f59ba33731584a7af809fdd68592e
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-04-12 08:18:16 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,10 +1,12 @@
 def solution(my_str, n):
     answer = []
     while True:
+        # 문자열 개수가 n 보다 작아지면 남은 문자열 삽입 후 while 종료
         if len(my_str) <= n:
             answer.append(my_str)
             break
         else:
+            # n 개의 문자열 삽입 후 n개를 제외한 나머지 문자열로 변환
             answer.append(my_str[:n])
             my_str = my_str[n:]
     return answer
