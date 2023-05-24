e8b566a71e0d2966381cf58a8f1179fa567891ee
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-05-24 19:56:44 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,6 +1,6 @@
 def solution(a, b, n):
     answer = 0
-    while (n >= a): # 총 콜라가 빈병교환 개수보다 작거나 같을 때 반복 종료
+    while n >= a: # 총 콜라가 빈병교환 개수보다 작거나 같을 때 반복 종료
         bottle = n % a # 빈병 교체 할수 있는 수량을 제외한 수
         n = (n//a) * b # 새로 받는 콜라의 수
         answer += n 
